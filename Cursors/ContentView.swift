2f6718bd332a689f7829b01ffa28a72b4115df1b
Philip Davis <philipcdavis11@gmail.com>
2021-01-26 10:43:18 -0500

    Initial Commit

---
+++ Cursors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cursors
+//
+//  Created by Philip Davis on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cursors/CursorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorsApp.swift
+//  Cursors
+//
+//  Created by Philip Davis on 1/26/21.
+//
+
+import SwiftUI
+
+@main
+struct CursorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
